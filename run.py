8c0da90295aac4db44a86aeea519b9113c07a9a8
Viki Mulhall <vikimulhall@gmail.com>
2024-10-02 10:01:31 +0000

    Print sales and stock rows

--- run.py
+++ run.py
@@ -70,7 +70,8 @@ def calculate_surplus_data(sales_row):
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    print(stock_row)
+    print(f"stock_row: {stock_row}")
+    print(f"sales row: {sales_row}")
 
 def main():
     """
